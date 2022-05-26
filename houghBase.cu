b1cbeb5b1327056f286753149e404778c4acc216
virtualmonkey <luis212urbinau@gmail.com>
2022-05-25 19:51:55 -0600

    Added gloID calculation with CUDA

--- houghBase.cu
+++ houghBase.cu
@@ -73,7 +73,8 @@ void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
 __global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float *d_Cos, float *d_Sin)
 {
   //TODO calcular: int gloID = ?
-  int gloID = w * h + 1; //TODO
+  // int gloID = w * h + 1; //TODO
+  int gloID = (blockIdx.x) * blockDim.x + threadIdx.x;
   if (gloID > w * h) return;      // in case of extra threads in block
 
   int xCent = w / 2;
