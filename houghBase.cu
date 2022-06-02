dfbc112b4c8897f2313b1613cf23fc7c986e25a3
virtualmonkey <luis212urbinau@gmail.com>
2022-06-01 23:41:48 -0600

    back to iteration 1

--- houghBase.cu
+++ houghBase.cu
@@ -53,8 +53,8 @@ void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
 //*****************************************************************
 // TODO usar memoria constante para la tabla de senos y cosenos
 // inicializarlo en main y pasarlo al device
-__constant__ float d_Cos[degreeBins];
-__constant__ float d_Sin[degreeBins];
+//__constant__ float d_Cos[degreeBins];
+//__constant__ float d_Sin[degreeBins];
 
 //*****************************************************************
 //TODO Kernel memoria compartida
@@ -70,11 +70,11 @@ __constant__ float d_Sin[degreeBins];
 
 // GPU kernel. One thread per image pixel is spawned.
 // The accummulator memory needs to be allocated by the host in global memory
-__global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale)
+__global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float *d_Cos, float *d_Sin)
 {
   //TODO calcular: int gloID = ?
-  int gloID = (blockIdx.x) * blockDim.x +  threadIdx.x;
-  //int gloID = w * h + 1; //TODO
+  // int gloID = w * h + 1; //TODO
+  int gloID = (blockIdx.x) * blockDim.x + threadIdx.x;
   if (gloID > w * h) return;      // in case of extra threads in block
 
   int xCent = w / 2;
@@ -119,13 +119,11 @@ int main (int argc, char **argv)
   int w = inImg.x_dim;
   int h = inImg.y_dim;
 
-  // we can delete the declaration here because these variables are global
-  // float* d_Cos;
-  // float* d_Sin;
+  float* d_Cos;
+  float* d_Sin;
 
-  // we allocate the variables in a different manner
-  cudaMallocManaged ((void **) &d_Cos, sizeof (float) * degreeBins);
-  cudaMallocManaged ((void **) &d_Sin, sizeof (float) * degreeBins);
+  cudaMalloc ((void **) &d_Cos, sizeof (float) * degreeBins);
+  cudaMalloc ((void **) &d_Sin, sizeof (float) * degreeBins);
 
   // CPU calculation
   CPU_HoughTran(inImg.pixels, w, h, &cpuht);
@@ -144,9 +142,9 @@ int main (int argc, char **argv)
   float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
   float rScale = 2 * rMax / rBins;
 
-  // Now we turn this into global memory
-  cudaMemcpyToSymbol(d_Cos, pcCos, sizeof (float) * degreeBins);
-  cudaMemcpyToSymbol(d_Sin, pcSin, sizeof (float) * degreeBins);
+  // TODO eventualmente volver memoria global
+  cudaMemcpy(d_Cos, pcCos, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_Sin, pcSin, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
 
   // setup and copy data from host to device
   unsigned char *d_in, *h_in;
@@ -166,7 +164,7 @@ int main (int argc, char **argv)
   int blockNum = ceil (w * h / 256);
 
   cudaEventRecord(start);
-  GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale);
+  GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
 
   // get results from device
   cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
@@ -186,10 +184,10 @@ int main (int argc, char **argv)
   cudaEventElapsedTime(&milliseconds, start, stop);
   printf ("Time in milliseconds: %f \n", milliseconds);
 
-  free(pcCos);
-  free(pcSin);
   cudaFree(d_Cos);
   cudaFree(d_Sin);
+  cudaFree(pcCos);
+  cudaFree(pcSin);
   cudaFree(h_hough);
   cudaFree(d_in);
   cudaFree(d_hough);
